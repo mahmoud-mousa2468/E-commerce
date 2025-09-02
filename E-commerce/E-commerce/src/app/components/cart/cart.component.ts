22ded2bbc24e1d791521a9fcc891c4ba1f4c53ae
mahmoud-mousa2468 <mo.mousa2468@gmail.com>
2025-09-02 14:48:37 +0200

    secondone

--- E-commerce/E-commerce/src/app/components/cart/cart.component.ts
+++ E-commerce/E-commerce/src/app/components/cart/cart.component.ts
@@ -2,29 +2,71 @@ import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { CartService } from '../../core/services/cart.service';
 import { Subscription } from 'rxjs';
 import { ICart } from '../../core/interfaces/icart';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
 })
-export class CartComponent implements OnInit,OnDestroy{
+export class CartComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.theUnscbscribeValue.unsubscribe;
   }
-  private readonly _CartService=inject(CartService)
-  userCarts:ICart={} as ICart
-  theUnscbscribeValue!:Subscription;
+  private readonly _CartService = inject(CartService);
+  cartDetails: ICart = {} as ICart;
+  theUnscbscribeValue!: Subscription;
   ngOnInit(): void {
-    this.theUnscbscribeValue=this._CartService.getLoggedUserCart().subscribe({
+    this.theUnscbscribeValue = this._CartService.getLoggedUserCart().subscribe({
+      next: (res) => {
+        console.log(res.data);
+        this.cartDetails = res.data;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+  removeItem(id: any): void {
+    this.theUnscbscribeValue = this._CartService
+      .removeSpecificCartItem(id)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.cartDetails = res.data;
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+  }
+  updateCount(id: any, countNum: number): void {
+    if (countNum > 0) {
+      this.theUnscbscribeValue = this._CartService
+        .updateCartProductQuantity(id, countNum)
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            this.cartDetails = res.data;
+          },
+          error: (err) => {
+            console.log(err);
+          },
+        });
+    }
+  }
+  clearCart():void{
+    this.theUnscbscribeValue=this._CartService.clearUserCart().subscribe({
       next:(res)=>{
-        console.log(res.data)
+        console.log(res)
+        if(res.message=='success'){
+        this.cartDetails={}as ICart
+        }
       },error:(err)=>{
         console.log(err)
       }
     })
   }
-
 }
--- E-commerce/E-commerce/src/app/core/services/cart.service.ts
+++ E-commerce/E-commerce/src/app/core/services/cart.service.ts
@@ -1,25 +1,49 @@
 import { HttpClient } from '@angular/common/http';
-import {Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
-
-  constructor(private _HttpClient:HttpClient) { }
-  myHeaders:any={token:localStorage.getItem('userToken')}
-  addProductToCart(id:any):Observable<any>{
-return this._HttpClient.post(`${environment.baseUrl}cart`,{
-  "productId":id
-},{
-headers:this.myHeaders
-})
+  constructor(private _HttpClient: HttpClient) {}
+  myHeaders: any = { token: localStorage.getItem('userToken') };
+  addProductToCart(id: any): Observable<any> {
+    return this._HttpClient.post(
+      `${environment.baseUrl}cart`,
+      {
+        productId: id,
+      },
+      {
+        headers: this.myHeaders,
+      }
+    );
   }
-getLoggedUserCart():Observable<any>{
-return  this._HttpClient.get(`${environment.baseUrl}cart`,{
-  headers:this.myHeaders
-})
-}
+  getLoggedUserCart(): Observable<any> {
+    return this._HttpClient.get(`${environment.baseUrl}cart`, {
+      headers: this.myHeaders,
+    });
+  }
+  removeSpecificCartItem(id: any): Observable<any> {
+    return this._HttpClient.delete(`${environment.baseUrl}cart/${id}`, {
+      headers: this.myHeaders,
+    });
+  }
+  updateCartProductQuantity(id: any, countNum: number): Observable<any> {
+    return this._HttpClient.put(
+      `${environment.baseUrl}cart/${id}`,
+      {
+        count: countNum,
+      },
+      {
+        headers: this.myHeaders,
+      }
+    );
+  }
+  clearUserCart(): Observable<any> {
+    return this._HttpClient.delete(`${environment.baseUrl}cart`, {
+      headers: this.myHeaders
+    });
+  }
 }
